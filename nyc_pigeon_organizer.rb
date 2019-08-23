1590428502dd4ae094fd6fae5bef2c8c04428293
Ashwin Kannan <ashwin.kannan@berkeley.edu>
2019-08-23 20:21:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,9 @@ def nyc_pigeon_organizer(data)
   data.reduce({}) { |hash, (features, values)|
     values.reduce(hash) { |hash, (feature, pigeons)|
       pigeons.each { |pigeon|
+        if !hash[pigeon]
+          hash[pigeon] = {}
+        end
         if hash[pigeon][features]
           hash[pigeon][features].push(feature)
         else
